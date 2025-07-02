993d1c1f3a086dc686b64531e2fcb720988d9155
Dora Lee <dsa28s@naver.com>
2025-07-02 19:30:54 +0900

    package: 패키지 업데이트 (arm64 iOS 시뮬레이션 지원)

--- Package.swift
+++ Package.swift
@@ -19,43 +19,43 @@ let package = Package(
         ),
         .binaryTarget(
             name: "ffmpegkit",
-            url: "https://github.com/dsa28s/ffmpeg-ios-spm/releases/download/20250702_155432/ffmpegkit-ios@20250702_170000.zip",
-            checksum: "8ead116d9f8492b61352890b241750e2bda857039a6054b0111e5d440235ce82"
+            url: "https://github.com/dsa28s/ffmpeg-ios-spm/releases/download/20250702_192134/ffmpegkit-ios@20250702_192134.zip",
+            checksum: "d8a48b83d0707f435a22d1ef09e8121031b197c120315807b0e2a7bc008f8a36"
         ),
         .binaryTarget(
             name: "libavcodec",
-            url: "https://github.com/dsa28s/ffmpeg-ios-spm/releases/download/20250702_155432/libavcodec-ios@20250702_170000.zip",
-            checksum: "f28f127a1b7200a9746e62706f19b37b96b5ad9de3d8c91172eb888e8d0b40d0"
+            url: "https://github.com/dsa28s/ffmpeg-ios-spm/releases/download/20250702_192134/libavcodec-ios@20250702_192134.zip",
+            checksum: "bba9f948c4180ea2508a8476048496b8cb00ffc87fa2bad2771cfd56e4b8d99d"
         ),
         .binaryTarget(
             name: "libavdevice",
-            url: "https://github.com/dsa28s/ffmpeg-ios-spm/releases/download/20250702_155432/libavdevice-ios@20250702_170000.zip",
-            checksum: "00b11417fe92bfdd5e97198dd59816ba04f03ffea8213f182de177b3d168a62b"
+            url: "https://github.com/dsa28s/ffmpeg-ios-spm/releases/download/20250702_192134/libavdevice-ios@20250702_192134.zip",
+            checksum: "f9a8acb21ef6c496c536a19d1a86d7c3ffb1cc61186eed4c87480139b33c8fb5"
         ),
         .binaryTarget(
             name: "libavfilter",
-            url: "https://github.com/dsa28s/ffmpeg-ios-spm/releases/download/20250702_155432/libavfilter-ios@20250702_170000.zip",
-            checksum: "11b31eab68de4475d3262c26395acf5e824fa1b9141fc494cd0c30c8b49cc646"
+            url: "https://github.com/dsa28s/ffmpeg-ios-spm/releases/download/20250702_192134/libavfilter-ios@20250702_192134.zip",
+            checksum: "b751039ee73ef5782b760e50a30b7032da22e5b1f4b92eff9636eb18a9ff9b97"
         ),
         .binaryTarget(
             name: "libavformat",
-            url: "https://github.com/dsa28s/ffmpeg-ios-spm/releases/download/20250702_155432/libavformat-ios@20250702_170000.zip",
-            checksum: "80211181e1d12f3004ab30c4e97d7466cde01dd5a3ede61a6bd098475e75484a"
+            url: "https://github.com/dsa28s/ffmpeg-ios-spm/releases/download/20250702_192134/libavformat-ios@20250702_192134.zip",
+            checksum: "eacd60ae8c6ae6b3054a6d8b8fc3161bf82c71d823fb8e44196a17fdb5311d80"
         ),
         .binaryTarget(
             name: "libavutil",
-            url: "https://github.com/dsa28s/ffmpeg-ios-spm/releases/download/20250702_155432/libavutil-ios@20250702_170000.zip",
-            checksum: "e72f70a0326fb09e41f51877580d19e61bf13e886e485cba7cf04fdfd8112d00"
+            url: "https://github.com/dsa28s/ffmpeg-ios-spm/releases/download/20250702_192134/libavutil-ios@20250702_192134.zip",
+            checksum: "27b0128c2bfddc7d5393bfcdbefccb214c97d06b300b0004c94721a8700f304a"
         ),
         .binaryTarget(
             name: "libswresample",
-            url: "https://github.com/dsa28s/ffmpeg-ios-spm/releases/download/20250702_155432/libswresample-ios@20250702_170000.zip",
-            checksum: "87175c63d85bf2680aacc7bc91fea3e7c31d9b778a6582b7d27866d8b489dab7"
+            url: "https://github.com/dsa28s/ffmpeg-ios-spm/releases/download/20250702_192134/libswresample-ios@20250702_192134.zip",
+            checksum: "85e580279e44da52cab1fb2bc8572308593397fc45b18c42a085495c2a9cb59b"
         ),
         .binaryTarget(
             name: "libswscale",
-            url: "https://github.com/dsa28s/ffmpeg-ios-spm/releases/download/20250702_155432/libswscale-ios@20250702_170000.zip",
-            checksum: "8e1dc21c376fe9cd42a0e9fec2a80d11d3455725995d0b1aac2dafdcef333c4a"
+            url: "https://github.com/dsa28s/ffmpeg-ios-spm/releases/download/20250702_192134/libswscale-ios@20250702_192134.zip",
+            checksum: "04ffb65d50686b84f63f1ef62ec0acbcb96b2ef70c412c3cb564005473f4fc63"
         ),
     ]
 )
